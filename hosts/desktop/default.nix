d7bacc81125bf35e5944a4c644b2399c56c3b7c8
db <dillon.barrera@gmail.com>
2025-06-09 18:38:54 -0500

    Added firewall config to desktop/default.nix

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -79,7 +79,7 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 4444 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
