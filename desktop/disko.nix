baed406ebdcd25919f11cd643566fca9f4064dcd
db <dillon.barrera@gmail.com>
2025-07-01 01:02:12 -0500

    Modify /desktop/disko.nix

--- desktop/disko.nix
+++ desktop/disko.nix
@@ -1,44 +1,71 @@
-{ disks ? throw "Missing argument 'disks'. Set it explicitly, e.g.: disks = [ '/dev/sda' ]", ... }: {
+{ disks ? throw "Missing argument 'disks'. Set it explicitly, e.g.: disks = [ '/dev/sda' ]", ... }:
+{
   disko.devices = {
     disk = {
-     changeme-eg-disk-main-esp = {
+      aconite = {
         type = "disk";
         device = builtins.elemAt disks 0;
         content = {
           type = "gpt";
           partitions = {
-            esp = {
+            ESP = {
               priority = 1;
-              name = "ESP";
               size = "1G";
               type = "EF00";
-            #uuid = "";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              extraArgs = [ "-F32" ];
-              mountpoint = "/boot";
-              mountOptions = [ "umask=0077" ];
-            };
-          };
-          luks = {
-            priority = 2;
-            name = "LUKS";
-            size = "100%";
-            type = "8E00";
-            #uuid = "";
-            content = {
-              type = "luks";
-              name = "crypted";
-              extraOpenArgs = [ ];
-              #passwordFile = "/tmp/secret.key";
-              settings = {
-                allowDiscards = true;
-                #keyFile = "/tmp/secret.key";
-              };
+              #uuid = "";
               content = {
-                type = "lvm_pv";
-                vg = "vg_aconite";
+                type = "filesystem";
+                format = "vfat";
+                extraArgs = [ "-F32" ];
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            luks = {
+              priority = 2;
+              size = "100%";
+              #uuid = "";
+              content = {
+                type = "luks";
+                name = "crypt";
+                extraOpenArgs = [ ];
+                #passwordFile = "/tmp/secret.key";
+                settings = {
+                  allowDiscards = true;
+                  #keyFile = "/tmp/secret.key";
+                };
+                content = {
+                  type = "lvm_pv";
+                  vg = "vg_aconite";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      vg_aconite = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%";
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "@" = {
+                  mountpoint = "/";
+                  mountOptions = [ "compress=zstd:1" "noatime" "space_cache=v2" "discard=async" "ssd" ];
+                };
+                "@nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [ "compress=zstd:1" "noatime" "space_cache=v2" "discard=async" "ssd" ];
+                };
+                "@persist" = {
+                  mountpoint = "/persist";
+                  mountOptions = [ "compress=zstd:1" "noatime" "space_cache=v2" "discard=async" "ssd" ];
+                };
               };
             };
           };
@@ -46,33 +73,6 @@
       };
     };
   };
-  lvm_vg = {
-    vg_aconite = {
-      type = "lvm_vg";
-      lvs = {
-        root = {
-          size = "100%";
-          content = {
-            type = "btrfs";
-            extraArgs = [ "-f" ];
-            subvolumes = {
-              "@" = {
-                mountpoint = "/";
-                mountOptions = [ "compress=zstd:1" "noatime" "space_cache=v2" "discard=async" "ssd" ];
-              };
-              "@nix" = {
-                mountpoint = "/nix";
-                mountOptions = [ "compress=zstd:1" "noatime" "space_cache=v2" "discard=async" "ssd" ];
-              };
-              "@home" = {
-                mountpoint = "/home";
-                mountOptions = [ "compress=zstd:1" "noatime" "space_cache=v2" "discard=async" "ssd" ];
-              };
-            };
-          };
-        };
-      };
-    };
-  };
-};
 }
+
+
