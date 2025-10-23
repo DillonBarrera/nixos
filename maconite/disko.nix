0e5a48f5e58727ba9162cf63cfb7190b48e65c5a
DillonBarrera <133410512+DillonBarrera@users.noreply.github.com>
2025-10-23 11:20:28 -0500

    Update disko.nix

--- maconite/disko.nix
+++ maconite/disko.nix
@@ -30,24 +30,10 @@
                 extraOpenArgs = [ ];
                 settings = {
                   allowDiscards = true;
-                  keyFile = "/run/keys/hdd.key";
-                  fallbackToPassword = true;
-                  preOpenCommands = ''
-                    sleep 1
-                    for i in $(seq 1 30); do
-                      if mount -n -t ext4 -o ro,noatime,nodev,nosuid /dev/disk/by-partuuid/3eedfb3e-f7b0-4ca3-8712-6dd6f56fea81 /run/keys; then
-                        break
-                      fi
-                      sleep 1
-                    done
-                  '';
-                  postOpenCommands = ''
-                    umount -n /run/keys || true
-                  '';
                 };
                 content = {
                   type = "lvm_pv";
-                  vg = "vg_aconite";
+                  vg = "vg_maconite";
                 };
               };
             };
@@ -56,7 +42,7 @@
       };
     };
     lvm_vg = {
-      vg_aconite = {
+      vg_maconite = {
         type = "lvm_vg";
         lvs = {
           root = {
