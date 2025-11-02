55e23d3bd6eabc5be48cd0f9d73b598855f5d264
db <dillon.barrera@gmail.com>
2025-11-01 21:34:35 -0500

    mango config update: click_method

--- modules/home-manager/programs/mango.nix
+++ modules/home-manager/programs/mango.nix
@@ -104,8 +104,9 @@
       trackpad_natural_scrolling=0
       disable_while_typing=1
       left_handed=0
-      middle_button_emulation=1
+      middle_button_emulation=0
       swipe_min_threshold=1
+      click_method=2
 
       # mouse
       # need relogin to make it apply
